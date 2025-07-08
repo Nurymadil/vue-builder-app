d1e8dc785556d209acfddec3327a9db6a7b0d429
Nurym <54814137+Nurymadil@users.noreply.github.com>
2025-07-08 22:30:57 +0500

    Add builder module with canvas and inspector components

---
+++ src/builder/entities/TextField.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <input class="text-field" type="text" placeholder="Text Field" />
+</template>
+
+<style scoped>
+.text-field {
+  padding: 0.25rem;
+}
+</style>
---
+++ src/builder/index.ts
@@ -0,0 +1,3 @@
+export * from './canvas'
+export * from './inspector'
+export * from './entities'
